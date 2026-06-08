8d4c07344f76a7c3fc52cacc4f021108567e2947
Sohil Desai <sohildesai@usf.edu>
2026-06-07 23:37:01 -0400

    Add switch logic for category discounts

--- cc_4.js
+++ cc_4.js
@@ -31,3 +31,24 @@ let products = [
     inventory: 15
   }
 ];
+for (let product of products) {
+  switch (product.category) {
+    case "electronics":
+      product.discountRate = 0.20;
+      break;
+
+    case "apparel":
+      product.discountRate = 0.15;
+      break;
+
+    case "groceries":
+    case "household":
+      product.discountRate = 0.10;
+      break;
+
+    default:
+      product.discountRate = 0;
+  }
+
+  product.discountedPrice = product.price * (1 - product.discountRate);
+}
